138d12624fb45161d7ba240a9cf7bd280c234b68
Darya Melgunova <darya.melgunova@gmail.com>
2024-12-25 23:03:07 +0300

    вввув

---
+++ src/test/java/TestBase.java
@@ -0,0 +1,30 @@
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+
+public class TestBase {
+    public static final String REPOSITORY = "Darya04031996/Allure_Reports";
+    public static final String TITLE = "Welcome to issues!";
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.baseUrl = "https://github.com/";
+        Configuration.browserSize = "1980x1080";
+        Configuration.pageLoadStrategy = "eager";
+
+    }
+
+    @BeforeEach
+    void beforeEach() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+
+    @AfterEach
+    void closeWebDriver() {
+        Selenide.closeWebDriver();
+    }
+}
