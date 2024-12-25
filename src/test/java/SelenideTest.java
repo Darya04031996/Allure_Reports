5fe9ba07954e3f5ef16ef8b777200ad6e7a37a8b
Darya Melgunova <darya.melgunova@gmail.com>
2024-12-25 22:15:14 +0300

    вввув

--- src/test/java/SelenideTest.java
+++ src/test/java/SelenideTest.java
@@ -1,7 +1,5 @@
-package qa.guru.allure;
-
 import com.codeborne.selenide.Condition;
-import org.testng.annotations.Test;
+import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.$;
